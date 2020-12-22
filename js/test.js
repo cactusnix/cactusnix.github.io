1bee3820a806fbc7ba409f33876954ffa3850cc7
cactusnix <cocalemontea@gmail.com>
2020-12-22 12:46:51 +0800

    remove old version

--- js/test.js
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-function switchTheme() {
-  let de = document.documentElement
-  let isDark = de.getAttribute('theme') == 'dark'
-  de.setAttribute('theme', isDark ? 'light' : 'dark')
-}
